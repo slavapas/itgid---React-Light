5762e1f889fefc42632fc49485d97787c510a627
Slava Pascaru <slavapascaru@gmail.com>
2022-04-30 21:19:19 +0100

    2nd commit

--- scripts.js
+++ scripts.js
@@ -10,8 +10,23 @@ class Header {
     out += `<h2>${this.h2}</h2>`
     document.getElementById(x).innerHTML = out
   }
+
+}
+
+// ==================================
+
+class Header2 extends Header {
+  constructor(h1, h2, hero) {
+    super(h1, h2) // наследует все свойства и методы предыдущего обьекта с переменными h1 и h2
+    this.hero = hero
+  }
+
 }
 
 const header = new Header('Hello', 'My site')
 header.render('root')
 console.log(header)
+
+const header2 = new Header2('Hi', 'Folks, you are ok!', 'Man, you look cool')
+header2.render('root2')
+console.log(header2)
